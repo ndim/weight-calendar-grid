5afc168445f2507806a25af0c8d13a451c72a567
Hans Ulrich Niedermann <hun@n-dimensional.de>
2016-01-19 22:41:21 +0100

    GUI test: fail gracefully in non GUI environment

--- weight_cal_grid/tests/test_gui.py
+++ weight_cal_grid/tests/test_gui.py
@@ -10,45 +10,47 @@ from unittest import TestCase
 ########################################################################
 
 
-from ..version import package_name, package_version
+try:
+    from ..gui import main as gui_main
 
+    from ..version import package_name, package_version
 
-########################################################################
+    class TestGui(TestCase):
 
+        def test_000_nothing(self):
+            pass
 
-class TestGui(TestCase):
+        def __test(self, args, expect_code=0, expect_stdout=None, expect_stderr=None):
+            # The GObject/Gtk Magic does not let us call gui.main(args)
+            # with the arguments to test, so we must call the GUI in a
+            # separate process.
+            with subprocess.Popen([sys.executable,
+                                   'wcg-gui'] + args,
+                                  stdin=subprocess.DEVNULL,
+                                  stderr=subprocess.PIPE,
+                                  stdout=subprocess.PIPE,
+                                  shell=False) as proc:
+                try:
+                    outs, errs = proc.communicate(timeout=15)
+                except subprocess.TimeoutExpired:
+                    proc.kill()
+                    outs, errs = proc.communicate(timeout=15)
+                self.assertEqual(proc.returncode, expect_code)
+                if expect_stderr != None:
+                    self.assertEqual(errs.decode(), expect_stderr)
+                if expect_stdout != None:
+                    self.assertEqual(outs.decode(), expect_stdout)
 
-    def test_000_nothing(self):
-        pass
+        def test_001_version(self):
+            self.__test(['--version'],
+                        expect_stdout=('wcg-gui (%s) %s\n' %
+                                       (package_name, package_version)))
 
-    def __test(self, args, expect_code=0, expect_stdout=None, expect_stderr=None):
-        # The GObject/Gtk Magic does not let us call gui.main(args)
-        # with the arguments to test, so we must call the GUI in a
-        # separate process.
-        with subprocess.Popen([sys.executable,
-                               'wcg-gui'] + args,
-                              stdin=subprocess.DEVNULL,
-                              stderr=subprocess.PIPE,
-                              stdout=subprocess.PIPE,
-                              shell=False) as proc:
-            try:
-                outs, errs = proc.communicate(timeout=15)
-            except subprocess.TimeoutExpired:
-                proc.kill()
-                outs, errs = proc.communicate(timeout=15)
-            self.assertEqual(proc.returncode, expect_code)
-            if expect_stderr != None:
-                self.assertEqual(errs.decode(), expect_stderr)
-            if expect_stdout != None:
-                self.assertEqual(outs.decode(), expect_stdout)
+        def test_002_help(self):
+            self.__test(['--help'])
 
-    def test_001_version(self):
-        self.__test(['--version'],
-                    expect_stdout=('wcg-gui (%s) %s\n' %
-                                   (package_name, package_version)))
-
-    def test_002_help(self):
-        self.__test(['--help'])
+except ImportError:
+    pass
 
 
 ########################################################################
